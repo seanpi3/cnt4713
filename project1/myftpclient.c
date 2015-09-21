68133f67c80d17f992750331a11918922e199825
Sean <spiamartino@gmail.com>
2015-09-21 15:53:17 -0400

    Initial commit

---
+++ project1/myftpclient.c
@@ -0,0 +1,76 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+void syserr(char* msg) { perror(msg); exit(-1); }
+
+int main(int argc, char* argv[])
+{
+  int sockfd, portno, n;
+  struct hostent* server;
+  struct sockaddr_in serv_addr;
+  char buffer[256];
+
+  if(argc != 3) {
+    fprintf(stderr, "Usage: %s <hostname> <port>\n", argv[0]);
+    return 1;
+  }
+  server = gethostbyname(argv[1]);
+  if(!server) {
+    fprintf(stderr, "ERROR: no such host: %s\n", argv[1]);
+    return 2;
+  }
+  portno = atoi(argv[2]);
+  
+  /*{
+  struct in_addr **addr_list; int i;
+  printf("Official name is: %s\n", server->h_name);
+  printf("    IP addresses: ");
+  addr_list = (struct in_addr **)server->h_addr_list;
+  for(i = 0; addr_list[i] != NULL; i++) {
+    printf("%s ", inet_ntoa(*addr_list[i]));
+  }
+  printf("\n");
+  }*/
+
+  sockfd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+  if(sockfd < 0) syserr("can't open socket");
+  //printf("create socket...\n");
+
+  memset(&serv_addr, 0, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr = *((struct in_addr*)server->h_addr);
+  serv_addr.sin_port = htons(portno);
+
+  if(connect(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0)
+    syserr("can't connect to server");
+  printf("Connection to server at %s:%s established. Now awaiting command...\n", argv[1], argv[2]);
+for(;;){  
+  printf("%s:%s> ",argv[1],argv[2]);
+  fgets(buffer, 255, stdin);
+  n = strlen(buffer); 
+  if(n>0 && buffer[n-1] == '\n') buffer[n-1] = '\0';
+
+  n = send(sockfd, buffer, strlen(buffer), 0);
+  if(strcmp(buffer,"quit")==0 || strcmp(buffer,"stop")==0){
+ 	printf("Exit command recieved\n");
+	close(sockfd);
+	exit(0);
+   }
+  if(n < 0){
+  	printf ("Connection to the server %s:%s has been lost.", argv[1],argv[2]);
+  	exit(0);
+  }
+  n = recv(sockfd, buffer, 255, 0);
+  if(n < 0) syserr("can't receive from server");
+  else buffer[n] = '\0';
+  printf("%s\n", buffer);
+}
+  close(sockfd);
+  return 0;
+}
---
+++ project1/myftpserver.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <dirent.h>
+
+void syserr(char *msg) { perror(msg); exit(-1); }
+
+
+
+int main(int argc, char *argv[])
+{
+  int sockfd, newsockfd, portno, n;
+  struct sockaddr_in serv_addr, clt_addr;
+  socklen_t addrlen;
+  char buffer[256];
+  if(argc != 2) { 
+    fprintf(stderr,"Usage: %s <port>\n", argv[0]);
+    return 1;
+  } 
+  portno = atoi(argv[1]); //converts portno string to int
+
+  sockfd = socket(AF_INET, SOCK_STREAM, 0); //returns -1 if failed to open socket
+  if(sockfd < 0){
+	 close(sockfd);
+	 syserr("can't open socket");
+  }
+  printf("Socket created...");
+
+  memset(&serv_addr, 0, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  serv_addr.sin_addr.s_addr = INADDR_ANY;
+  serv_addr.sin_port = htons(portno);
+
+  if(bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
+	close(sockfd);
+	syserr("can't bind");
+  }
+  printf("Socket binded to port %d...\n", portno);
+
+  listen(sockfd, 5); 
+
+for(;;){
+  newsockfd = accept(sockfd, (struct sockaddr*)&clt_addr, &addrlen);
+  addrlen = sizeof(clt_addr); 
+  if(newsockfd < 0){
+	close(newsockfd);	
+ 	printf("Cannot accept client");
+  } 
+ printf("Client connected...\n");
+  int pid = fork();
+  if(pid<0) syserr("Error");
+  else if(pid == 0) {
+	close(newsockfd);
+  }
+  else{	
+	char *msg;
+	char *command;
+    for(;;){
+	int found = 0;
+  	n = recv(newsockfd, buffer, 255, 0); 
+  	if(n < 0) syserr("can't receive from client"); 
+  	else buffer[n] = '\0';
+	char *toke;  
+	printf("SERVER GOT MESSAGE: %s\n", buffer); 
+    	toke = strtok(buffer," ,.-");
+	command = "stop";
+	if(!found && strcmp(toke, command)==0){
+		msg = "Server stopping.\n";
+		n = send(newsockfd, msg, strlen(msg),0);
+		found = 1;
+		close(newsockfd);
+		close(sockfd);
+		printf("server stopping\n");
+		exit(0);
+	}
+	command = "ls-remote";
+	msg = "ls-remote";
+	if(!found && strcmp(toke,command)==0){
+		n = send(newsockfd,msg,strlen(msg),0);
+		printf("ls-remote\n");
+		found = 1;
+	}
+	command = "get";
+	msg = "get";
+	if(!found && strcmp(toke,command)==0){
+		n = send(newsockfd,msg,strlen(msg),0);
+		printf("getting\n");
+		found = 1;
+	}
+	command = "put";
+	msg = "putting";
+	if(!found && strcmp(toke,command)==0){
+		n = send(newsockfd,msg,strlen(msg),0);
+		printf("putting\n");
+		found = 1;
+	}
+	if(!found){
+		msg = "Invalid command. Please send one of the following valid commands: ls-remote, get <filename>, put <filename>, stop.";
+		n = send(newsockfd, msg, strlen(msg),0);
+	}
+    }
+  	close(newsockfd); 
+  }
+}
+  close(sockfd); 
+  return 0;
+}
