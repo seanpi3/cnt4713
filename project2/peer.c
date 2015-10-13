3ee79a9d1a9740b95a450ca0b7ec3082fdcaaed9
Sean <spiamartino@gmail.com>
2015-10-13 18:22:58 -0400

    update

--- project2/peer.c
+++ project2/peer.c
@@ -8,16 +8,61 @@
 #include <arpa/inet.h>
 #include <pthread.h>
 
+int serversockfd, serverport;
+struct sockaddr_in serv_addr;
 void syserr(char* msg) { perror(msg); exit(-1); }
-void* serverLogic(void *arg){
-  printf("server logic here\n");
+
+//Logic for the server thread
+void *serverLogic(void *arg){
+  int clientsockfd,n;
+  struct sockaddr_in clt_addr;
+  socklen_t addrlen;
+  if(bind(serversockfd, (struct sockaddr*)&serv_addr,sizeof(serv_addr)) < 0){
+    close(serversockfd);
+    syserr("can't bind");
+  }
+  listen(serversockfd,5);
+  for(;;){
+    clientsockfd = accept(serversockfd, (struct sockaddr*)&clt_addr,&addrlen);
+    addrlen = sizeof(clt_addr);
+    if(clientsockfd <0){
+	close(clientsockfd);
+	printf("Cannot accept client\n");
+    }
+    else{
+	printf("peer connected...\n");
+	int pid = fork();
+	if(pid<0) syserr("error");
+	else if (pid ==0) close(clientsockfd);
+	else{
+	  char buffer[256];
+	  char *filename;
+	  char *toke;
+	  int filesize;
+	  for(;;){
+	    memset(buffer,0,sizeof(buffer));
+	    n = recv(clientsockfd,buffer,sizeof(buffer),0);
+	    if(n<0) {
+		printf("Peer lost connection\n");
+		close(clientsockfd);
+		pthread_exit(NULL);
+	    }
+	    else buffer[n] = '\0';
+	    printf("SERVER GOT MESSAGE: %s\n",buffer);
+	  }
+	}
+    }
+  }
+  
+  
+  
 }
 
 int main(int argc, char* argv[])
 {
-  int trackersockfd,serversockfd,peersockfd,trackerport,peerport,serverport, n;
+  int trackersockfd,peersockfd,trackerport,peerport, n;
   struct hostent* tracker, peer;
-  struct sockaddr_in tracker_addr, peer_addr, serv_addr, clt_addr;
+  struct sockaddr_in tracker_addr, peer_addr;
   socklen_t addrlen;
   pthread_t servt;
   char buffer[256];
@@ -65,13 +110,15 @@ int main(int argc, char* argv[])
   //Create thread for the server
   n = pthread_create(&servt,NULL, &serverLogic, NULL);
   if(n<0) syserr("can't create thread");
-  else printf("Thread created successfully\n");
   
-
   //Connect to tracker  
   if(connect(trackersockfd, (struct sockaddr*)&tracker_addr, sizeof(tracker_addr)) < 0)
     syserr("can't connect to tracker");
   printf("Connected to tracker.\n");
+
+  for(;;){
+
+  }
   /*
   printf("PLEASE ENTER MESSAGE: ");
   fgets(buffer, 255, stdin);
--- project2/tracker.c
+++ project2/tracker.c
@@ -6,58 +6,63 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+int sockfd;
 void syserr(char *msg) { perror(msg); exit(-1); }
+void *trackerLogic(void *arg){
+  int clientsockfd, n;
+  socklen_t addrlen;
+  char buffer[256];
+  clientsockfd = sockfd;
+  printf("client connected\n");
+}
 
 int main(int argc, char *argv[])
 {
-  int sockfd, clientsockfd, portno, n;
+  int clientsockfd, portno, n;
   struct sockaddr_in serv_addr, clt_addr;
   socklen_t addrlen;
+  pthread_t clt_thread;
   char buffer[256];
-  //If port number argument is not given default to 5000
-  if(argc != 2) { 
-	portno = 5000;
-  } 
-  else portno = atoi(argv[1]);
 
+  //If port number argument is not given default to 5000
+  if(argc < 2) portno = 5000; 
+  else if(argc == 2) portno = atoi(argv[1]);
+  else fprintf(stderr, "Usage: %s <port>\n",argv[0]);
+
+  //Create server socket
   sockfd = socket(AF_INET, SOCK_STREAM, 0); 
   if(sockfd < 0) syserr("can't open socket"); 
   //printf("create socket...\n");
 
+  //Set up server address
   memset(&serv_addr, 0, sizeof(serv_addr));
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_addr.s_addr = INADDR_ANY;
   serv_addr.sin_port = htons(portno);
 
+  //Bind socket to port
   if(bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) 
     syserr("can't bind");
   //printf("bind socket to port %d...\n", portno);
 
   listen(sockfd, 5); 
-
+  printf("Tracker set up and waiting for clients...\n");
+//Accept and set up incoming clients
 for(;;) {
   //printf("wait on port %d...\n", portno);
-  addrlen = sizeof(clt_addr); 
   clientsockfd = accept(sockfd, (struct sockaddr*)&clt_addr, &addrlen);
-  if(clientsockfd < 0) syserr("can't accept"); 
-  printf("new incoming connection, block on receive\n");
-  pid = fork();
-  if(pid<0) syserr("fork error");
-  else if(pid == 0){
+  addrlen = sizeof(clt_addr); 
+  if(clientsockfd < 0){
     close(clientsockfd);
-  }
+    printf("Cannot accept client\n");
+  } 
   else{
-    for(;;){
-      n = recv(clientsockfd, buffer, 255, 0); 
-      if(n < 0) syserr("can't receive from client"); 
-      else buffer[n] = '\0';
-      printf("SERVER GOT MESSAGE: %s\n", buffer); 
-    }
+    printf("New incoming connection...");
+    
+    //Create thread for incoming client
+    n = pthread_create(&clt_thread,NULL,&trackerLogic, NULL);
+    if(n<0) syserr("can't create thread");
   }
-  //n = send(newsockfd, buffer, sizeof(buffer), 0);
-  //if(n < 0) syserr("can't send to server"); 
-  //printf("send message...\n");
-  close(newsockfd); 
 }
   close(sockfd); 
   return 0;
