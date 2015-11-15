3f442913d05869c10cebb55989e461d7122c319a
Sean <spiamartino@gmail.com>
2015-11-15 17:27:50 -0500

    mostly working sender moving on to receiver

--- project3/receiver.c
+++ project3/receiver.c
@@ -45,11 +45,22 @@ uint16_t calculateChecksum(void* data,size_t length){
 }
 
 void sendACK(int seq_num, int sockfd,struct sockaddr *dest_addr,socklen_t addrlen){
-  //n = sendto(sockfd, buffer, strlen(buffer), 0, (struct sockaddr*)&sdr_addr, addrlen);
+	char *ACK[sizeof(uint32_t)+sizeof(uint16_t)+3];
+	void *offset = ACK;
+	uint32_t seqOut = htonl((uint32_t)seq_num);
+	memcpy(offset,&seqOut,sizeof(uint32_t));
+	char *msg = "ACK";
+	uint16_t checkSum = calculateChecksum(&seqOut,sizeof(uint32_t));
+	checkSum += calculateChecksum(msg,sizeof(msg));
+	offset+=sizeof(uint32_t);
+	memcpy(offset,&checkSum,sizeof(uint16_t));
+	offset+=sizeof(uint16_t);
+	memcpy(offset,msg,sizeof(msg));
+  int n = sendto(sockfd,ACK,sizeof(ACK), 0, (struct sockaddr*)&dest_addr, addrlen);
 }
 
 int receivePacket(int sockfd,void* buffer,size_t length,struct sockaddr* sdr_addr,socklen_t* addrlen){
-	int n = 0,remaining = packetSize;
+	int n = 0,remaining = length;
 	void *offset = buffer;
 	while(remaining>0){
 		offset+=packetSize-remaining;
@@ -57,10 +68,15 @@ int receivePacket(int sockfd,void* buffer,size_t length,struct sockaddr* sdr_add
   	if(n < 0) return -1;
 		remaining-=n;
 	}
-	uint16_t check = calculateChecksum(buffer,packetSize);
-	if(check==0xffff) printf("Checksum verified... sending ACK.\n");
-	else printf("Data corrupted rerequesting packet.\n"); 
-	return 1;
+	uint16_t check = calculateChecksum(buffer,length);
+	if(check==0xffff){
+		printf("Checksum verified... sending ACK.\n");
+		return 1;
+	}
+	else{
+		printf("Data corrupted rerequesting packet.\n"); 
+		return 0;
+	}
 }
 
 int main(int argc, char *argv[])
@@ -68,7 +84,7 @@ int main(int argc, char *argv[])
   int sockfd, portno, n;
   struct sockaddr_in serv_addr, sdr_addr; 
   socklen_t addrlen;
-	char packet[sizeof(int)+sizeof(uint16_t)+1024];
+	char packet[sizeof(uint32_t)+sizeof(uint16_t)+1024];
 
   if(argc != 3) { 
     fprintf(stderr,"Usage: %s <port> <filename>\n", argv[0]);
@@ -87,18 +103,25 @@ int main(int argc, char *argv[])
   if(bind(sockfd, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) 
     syserr("can't bind");
   addrlen = sizeof(serv_addr); 
-	n = receivePacket(sockfd,packet,packetSize,(struct sockaddr*)&serv_addr,addrlen);
-	if(n<0) syserr ("fuck\n");
+	memset(packet,0,sizeof(packet));
+	n = receivePacket(sockfd,packet,sizeof(packet),(struct sockaddr*)&serv_addr,addrlen);
+	if(n<0) syserr ("failed to receive packet\n");
 	
 	void *offset = packet;
-	int seq_num;
-	memcpy(&seq_num,offset,sizeof(int));
-	offset+=sizeof(int);
+	uint32_t seq_num;
+	uint32_t seq_num_in;
+	memcpy(&seq_num_in,offset,sizeof(uint32_t));
+	seq_num = ntohl(seq_num_in);
+	offset+=sizeof(uint32_t);
 	offset+=sizeof(uint16_t);
-	char payload[1024];
-	memcpy(payload,offset,1024);
+	uint32_t sizeIn;
+	memcpy(&sizeIn,offset,sizeof(uint32_t));
+	uint32_t fileSize = ntohl(sizeIn);
+	//char payload[1024];
+	//memcpy(payload,offset,1024);
   //if(n < 0) syserr("can't send to server"); 
-
+	printf("Packet received.\nseqnum:%d, filesize: %d\n",seq_num,fileSize);
+	sendACK((int)seq_num,sockfd,(struct sockaddr*)&serv_addr,addrlen);
   close(sockfd); 
   return 0;
 }
--- project3/sender.c
+++ project3/sender.c
@@ -6,26 +6,45 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <sys/stat.h>
+#include <pthread.h>
 
+int sockfd;
 int windowSize = 100;
-int packetSize = 1024;
+int payloadSize = 1024;
 int timeout = 10;
+struct sockaddr_in recv_addr;
+socklen_t addrlen;
+
+typedef struct window{
+	int seq_num;
+	void *payload;
+	int receivedACK;
+	pthread_t packet_thread;
+	struct window *next;
+};
+
+struct window *head;
+struct window *tail;
 
 void syserr(char* msg) { perror(msg); exit(-1); }
-int receiveACK(int sockfd){
-	/*
-	char* buff;
-	int n = recv(sockfd,buff,3,0);
-	if(n<=0) syserr("lost connection to receiver");
-	if(strcmp(buff,"ACK"))	return -1;
-	*/
-	return 0;
+
+struct window* findPacket(int seq_num){
+	struct window *current = tail;
+	while(current!=NULL){
+		if(current->seq_num==seq_num){
+			return current;
+		}
+		current=current->next;
+	}
+	return NULL;
 }
+
 
 void printPacket(void* data,size_t length){
 	char* d = (char*) data;
-	size_t i = 0;
-	for(i;i+1<length;i+=2){
+	size_t i;
+	for(i=0;i+1<length;i+=2){
 		uint16_t word;
 		memcpy(&word,d+i,2);
 		printf("%x",word);
@@ -41,8 +60,8 @@ void printPacket(void* data,size_t length){
 uint16_t calculateChecksum(void* data,size_t length){
 	char* d = (char*) data;
 	uint16_t sum = 0;
-	size_t i = 0;
-	for(i;i+1<length;i+=2){
+	size_t i;
+	for(i=0;i+1<length;i+=2){
 		uint16_t word;
 		memcpy(&word,d+i,2);
 		sum+=word;
@@ -55,17 +74,38 @@ uint16_t calculateChecksum(void* data,size_t length){
 	return sum;
 }
 
+int receiveACK(int sockfd,void* buffer,size_t length,struct sockaddr* sdr_addr,socklen_t* addrlen){
+	int n = 0, remaining = length;
+	void *offset = buffer;
+	while(remaining>0){
+		offset+=length-remaining;
+		n = recvfrom(sockfd,offset,remaining,0,sdr_addr,&addrlen);
+		if(n<0) return -1;
+		remaining-=n;
+	}
+	uint16_t check = calculateChecksum(buffer,length);
+	if(check==0xffff){
+		printf("Checksum verified.. ACK received.\n");
+		return 1;
+	}
+	else{
+		printf("Data corrupted... ACK not received.\n");
+		return 0;
+	}
+}
+
 int sendPacket(int seqNum,void *data,size_t data_size, int sockfd,struct sockaddr *dest_addr,socklen_t dest_len){
-	char packet[sizeof(int)+sizeof(uint16_t)+data_size];
+	char packet[sizeof(uint32_t)+sizeof(uint16_t)+data_size];
 	uint16_t checksum;
 	void *offset = packet;
+	uint32_t seqNumOut = htonl((uint32_t)seqNum);
 	memset(packet,0,sizeof(packet));
-	checksum = calculateChecksum(&seqNum,sizeof(int));
+	checksum = calculateChecksum(&seqNumOut,sizeof(uint32_t));
 	checksum += calculateChecksum(data,data_size);
 	checksum = 0xffff - checksum;
 	//checksum = htons(checksum);
-	memcpy(offset,&seqNum,sizeof(int));
-	offset+=sizeof(int);
+	memcpy(offset,&seqNumOut,sizeof(uint32_t));
+	offset+=sizeof(uint32_t);
 	memcpy(offset,&checksum,sizeof(uint16_t));
 	offset+=sizeof(uint16_t);
 	memcpy(offset,data,data_size);
@@ -74,12 +114,52 @@ int sendPacket(int seqNum,void *data,size_t data_size, int sockfd,struct sockadd
 	return 1;
 }	
 
+void *ackLogic(void *arg){
+	char ACK[sizeof(uint32_t)+sizeof(uint16_t)+3];
+	void *offset;
+	fd_set readset;
+	int n;
+	uint32_t seq_num,seq_num_in;
+	uint16_t checkSum;
+	while(1){
+		memset(ACK,0,sizeof(ACK));
+		FD_ZERO(&readset);
+		FD_SET(sockfd,&readset);
+		n = select(sockfd + 1,&readset,NULL,NULL,NULL);
+		n = receiveACK(sockfd,ACK,sizeof(ACK),(struct sockaddr*)&recv_addr,addrlen);
+		if(n>0){
+			offset = ACK;
+			memcpy(&seq_num_in,offset,sizeof(uint32_t));
+			seq_num = ntohl(seq_num_in);
+			struct window *packet = findPacket((int)seq_num);
+			packet->receivedACK = 1;
+		}
+	}
+}
+
+void *packetLogic(void *arg){
+		struct window *packet = (struct window *)arg;
+		printf("Sending packet %d\n", packet->seq_num);
+		packet->receivedACK = 0;
+		fd_set set;
+		struct timeval timeout;
+		timeout.tv_sec = 0;
+		timeout.tv_usec = 10 * 1000;
+		int n;
+		do{
+			FD_ZERO(&set);
+			FD_SET(sockfd,&set);
+			n = select(sockfd+1,NULL,&set,NULL,NULL);
+			n = sendPacket(packet->seq_num,packet->payload,
+								payloadSize,sockfd,(struct sockaddr*)&recv_addr,addrlen);
+			n = select(1,NULL,NULL,NULL,&timeout);
+		} while(!packet->receivedACK);
+}
+
 int main(int argc, char* argv[])
 {
-  int sockfd, portno, n;
+  int portno, n;
   struct hostent* receiver;
-  struct sockaddr_in recv_addr;
-  socklen_t addrlen;
 	FILE *f;	
 	char *filename;
 	
@@ -110,14 +190,63 @@ int main(int argc, char* argv[])
   recv_addr.sin_port = htons(portno);
 	addrlen = sizeof(recv_addr);
 
-
- // n = recvfrom(sockfd, buffer, 255, 0, (struct sockaddr*)&recv_addr, &addrlen);
-
 	
-	char buffer[packetSize];
-	n = fread(buffer,packetSize,1,f);
-	if(n<=0) syserr("can't read file\n");
-	sendPacket(0,buffer,packetSize,sockfd,(struct sockaddr*)&recv_addr,addrlen);
+ // n = recvfrom(sockfd, buffer, 255, 0, (struct sockaddr*)&recv_addr, &addrlen);
+	struct stat st;
+	stat(filename,&st);
+	int filesize = st.st_size;
+	int num_packets = filesize/payloadSize;
+	if(filesize%payloadSize>0) num_packets++;
+	printf("filesize: %d\n",filesize);
+	int packets_completed = 0;
+	uint32_t nsize =htonl((uint32_t)filesize);
+	printf("Transfer will be completed in %d packets\n",num_packets);
+	
+	head = malloc(sizeof(struct window));
+	head->seq_num = 0;
+	head->payload = malloc(payloadSize);
+	memset(head->payload,0,payloadSize);
+	memcpy(head->payload,&nsize,sizeof(uint32_t));
+	n = pthread_create(&(head->packet_thread),NULL,&packetLogic,head);
+	if(n<0)syserr("unable to create threads");
+	tail = head;
+	int bytes_remaining=filesize, bytes_read;
+	struct timeval tv;
+	fd_set set;
+	pthread_t ackThread;
+	n = pthread_create(&ackThread,NULL,&ackLogic,NULL);
+	while(num_packets-packets_completed > 0){
+			if(head->seq_num - tail->seq_num < 100){
+				head->next = malloc(sizeof(struct window));
+				head->next->seq_num = head->seq_num + 1;
+				head = head->next;
+				head->payload = malloc(payloadSize);
+				if(bytes_remaining < payloadSize){
+					bytes_read = fread(head->payload,bytes_remaining,1,f);
+					if(bytes_read<=0) syserr("can't read file\n");
+				}
+				else{
+					bytes_read = fread(head->payload,payloadSize,1,f);
+					if(bytes_read<=0) syserr("can't read file\n");
+				}
+				
+				FD_ZERO(&set);
+				FD_SET(0,&set);
+				tv.tv_sec = 0;
+				tv.tv_usec = 10 * 1000;
+				select(1,NULL,NULL,NULL,&tv);
+
+				n = pthread_create(&(head->packet_thread),NULL,&packetLogic,head);
+				if(n<0)syserr("unable to create threads");
+				bytes_remaining-=bytes_read;
+			}
+			if(tail->receivedACK){
+				struct window *old = tail;
+				tail = tail->next;
+				free(old->payload);
+				free(old);
+			}
+	}
 	fclose(f);
   close(sockfd);
   return 0;
