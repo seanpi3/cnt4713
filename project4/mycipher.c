1f0b612968b6052a12279b8c34a70b1a5c8e40ce
sean <spiamartino@gmail.com>
2015-11-25 19:06:54 -0500

    getting started

--- project4/mycipher.c
+++ project4/mycipher.c
@@ -2,6 +2,35 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct bit_map{
+	unsigned int b1,b2,b3,b4,b5,b6,b7,b8,b9,b10;
+};
+struct bit_map init_key;
+struct bit_map init_vector;
+
+void set_init_key(char* key){
+	init_key.b1 = key[0];
+	init_key.b2 = key[1];
+	init_key.b3 = key[2];
+	init_key.b4 = key[3];
+	init_key.b5 = key[4];
+	init_key.b6 = key[5];
+	init_key.b7 = key[6];
+	init_key.b8 = key[7];
+	init_key.b9 = key[8];
+	init_key.b10 = key[9];
+}
+void set_init_vector(char* key){
+	init_vector.b1 = key[0];
+	init_vector.b2 = key[1];
+	init_vector.b3 = key[2];
+	init_vector.b4 = key[3];
+	init_vector.b5 = key[4];
+	init_vector.b6 = key[5];
+	init_vector.b7 = key[6];
+	init_vector.b8 = key[7];
+}
+
 int main(int argc, char* argv[]){
 	char init_key[10];
 	char init_vector[8];
@@ -40,6 +69,8 @@ int main(int argc, char* argv[]){
 			return 1;
 		}
 	}
+	set_init_key(argv[1]);
+	set_init_vector(argv[2]);
 	
 	return 0;
 }
